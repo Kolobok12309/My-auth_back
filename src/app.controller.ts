56322988c56da2d4d45a20bed5fe79d382661a20
Sova <sova123409@gmail.com>
2020-03-15 18:26:50 +0300

    Db connection and typeorm

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 
+import { ApiTags } from '@nestjs/swagger';
+
 import AppService from './app.service';
 
+@ApiTags('cats')
 @Controller()
 export default class AppController {
   constructor(private readonly appService: AppService) {}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 import AppModule from './app.module';
@@ -7,6 +8,15 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('MyAuth system')
+    .setDescription('The auth API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 
   if (module.hot) {
